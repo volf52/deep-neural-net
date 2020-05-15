c8fbaa9798f9c9f4c4cf3795d6c0b38f97c52893
Majd <majd.kharrat@gmail.com>
2020-05-15 17:55:09 +0300

    Merge branch 'master' of D:\ara-proje-dnn with conflicts.

--- mlpcode/test.py
+++ mlpcode/test.py
@@ -2,31 +2,42 @@ from mlpcode.activation import ActivationFuncs as af
 from mlpcode.loss import LossFuncs as lf
 from mlpcode.network import Network
 from mlpcode.utils import DATASETS, loadDataset, MODELDIR
-from mlpcode.optim import LRScheduler, LRSchedulerStrat as LRS
 
-useGpu = False
-binarized = False
+useGpu = True
 dataset = DATASETS.mnist
 print("Loading {}".format(dataset))
 trainX, trainY, testX, testY = loadDataset(dataset, useGpu=useGpu)
 print("Finished loading {} data".format(dataset))
-layers = [trainX.shape[1], 512, 10]
-epochs = 10
-batchSize = 600
-# lr = 0.07
-lr = LRScheduler(alpha=0.07, decay_rate=0.8, strategy=LRS.time)
+layers = [trainX.shape[1], 150, 300, 10]
+epochs = 50
+batchSize = 50
+lr = 7e-2
 print("Creating neural net")
 
 # Creating from scratch
-nn = Network(layers, useGpu=useGpu, binarized=binarized,)
-
-# Creating from a pretrained model
-modelPath = MODELDIR / "mnist_1589459230.202179.npz"
-assert modelPath.exists()
-# nn = Network.fromModel(modelPath, useGpu=useGpu, binarized=binarized,)
-
-# Must compile the model before trying to train it
-nn.compile(lr=lr, hiddenAf=af.leaky_relu, outAf=af.softmax, lossF=lf.cross_entropy)
+nn = Network(
+    layers,
+    useGpu=useGpu,
+    hiddenAf=af.sigmoid,
+    outAf=af.softmax,
+    lossF=lf.cross_entropy,
+    binarized=False,
+)
 
 # Save must be the name of the dataset, if we want to save the model
-nn.train(trainX, trainY, epochs, batch_size=batchSize, save=dataset)
+# nn.train(trainX, trainY, epochs, batch_size=batchSize, lr=lr, valX=testX, valY=testY, save=dataset)
+
+
+
+# Creating from a pretrained model
+modelPath = MODELDIR / "mnist_fp.npz"
+assert modelPath.exists()
+nn = Network.fromModel(
+    modelPath,
+    useGpu=useGpu,
+    hiddenAf=af.sigmoid,
+    outAf=af.softmax,
+    lossF=lf.cross_entropy,
+    binarized=True
+)
+nn.train(trainX, trainY, 30, batch_size=batchSize, lr=lr, valX=testX, valY=testY, save=dataset)
