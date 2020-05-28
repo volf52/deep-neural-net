Fix softmax derivative. Change AF derivative API Now, the derivative of the activation function returns delta instead of dZ. This allows us to skip some reduntant calculations (such as in the case of ReLU  and Leaky ReLU). Softmax derivative is also up to date now, and is independent from cross-entropy loss. Cross entropy loss can now be used with other activation functions as well

mlpcode/layers.py
@@ -89,7 +89,7 @@ class LinearLayer:
         n = delta.shape[0]
         if self.activation is not None and activDeriv:
             assert "a" in self.cache
-            delta = dA * self.afderiv(self.cache["a"])
+            delta = self.afderiv(dA, self.cache["a"])
 
         # batchnorm updates to delta
         if self.batchNorm:
@@ -177,8 +177,6 @@ if __name__ == "__main__":
     lossF = LOSS_FUNCS[loss]
     lossDeriv = LOSS_DERIVATES[loss]
 
-    lossXEntr = loss == lf.mse
-
     dataset = DATASETS.mnist
     trainX, trainY, testX, testY = loadDataset(dataset, useGpu=useGpu, encoded=True)
     testY = np.squeeze(testY)
@@ -187,8 +185,13 @@ if __name__ == "__main__":
     valY = trainY.argmax(axis=1)
 
     l1 = LinearLayer(256, 784, activation=af.leaky_relu, gpu=useGpu)
-    l2 = LinearLayer(10, 256, activation=af.sigmoid, gpu=useGpu)
+    l2 = LinearLayer(10, 256, activation=af.softmax, gpu=useGpu)
     layers = [l1, l2]
+
+    softCrossEntropy = loss == lf.cross_entropy and layers[-1].activation in (
+        af.softmax,
+        af.sigmoid,
+    )
 
     for layer in layers:
         layer.build()
@@ -203,10 +206,14 @@ if __name__ == "__main__":
             err = lossF(a, batchY).mean()
             epochLoss.append(err)
 
-            da = lossDeriv(a, batchY)
+            da = lossDeriv(a, batchY, with_softmax=softCrossEntropy)
+            da = layers[-1].backwards(da, lr, activDeriv=not softCrossEntropy)
 
-            for layer in reversed(layers):
+            for layer in reversed(layers[:-1]):
                 da = layer.backwards(da, lr)
 
         # print(sum(epochLoss) / len(epochLoss))
         print(accuracy(valX, valY, layers))
+
+    print("Test acc")
+    print(accuracy(testX, testY, layers))

mlpcode/loss.py
@@ -16,7 +16,7 @@ def cross_entropy_loss(ypred: cp.ndarray, y: cp.ndarray, eps=1e-15) -> cp.ndarra
     ypred
         Numpy array with predicted labels, with shape (num_instances, num_classes) and dtype: float32
     y
-        Numpy array with predicted labels, with shape (num_instances, num_classes) and dtype: uint8
+        Numpy array with actual labels, with shape (num_instances, num_classes) and dtype: uint8
     eps
         Tolerance for datapoints near zero (to prevent instability with log)
 
@@ -36,7 +36,9 @@ def cross_entropy_loss(ypred: cp.ndarray, y: cp.ndarray, eps=1e-15) -> cp.ndarra
     return L
 
 
-def cross_entropy_derivative(ypred: cp.ndarray, y: cp.ndarray, eps=1e-15) -> cp.ndarray:
+def cross_entropy_derivative(
+    ypred: cp.ndarray, y: cp.ndarray, eps=1e-15, with_softmax=False
+) -> cp.ndarray:
     """
     Derivative for Mean Squared Error
 
@@ -45,7 +47,7 @@ def cross_entropy_derivative(ypred: cp.ndarray, y: cp.ndarray, eps=1e-15) -> cp.
     ypred
         Numpy array with predicted labels, with shape (num_instances, num_classes) and dtype: float32
     y
-        Numpy array with predicted labels, with shape (num_instances,) and dtype: uint8
+        Numpy array with true labels, with shape (num_instances, num_classes) and dtype: uint8
 
     Returns
     -------
@@ -53,9 +55,14 @@ def cross_entropy_derivative(ypred: cp.ndarray, y: cp.ndarray, eps=1e-15) -> cp.
         The derivative of cross_entropy_loss(ypred, y) with shape (num_instances, num_classes)
     """
 
-    xp = cp.get_array_module(ypred)
-    # xp.clip(ypred, eps, 1 - eps, out=ypred)
-    dA = -(xp.divide(y, ypred) - xp.divide(1 - y, 1 - ypred))
+    if with_softmax:
+        dA = ypred - y
+
+    else:
+        xp = cp.get_array_module(ypred)
+        xp.clip(ypred, eps, 1 - eps, out=ypred)
+        dA = -(xp.divide(y, ypred) - xp.divide(1 - y, 1 - ypred))
+
     dA /= ypred.shape[0]
 
     return dA
@@ -70,7 +77,7 @@ def mse(ypred: cp.ndarray, y: cp.ndarray) -> cp.ndarray:
     ypred
         Numpy array with predicted labels, with shape (num_instances, num_classes) and dtype: float32
     y
-        Numpy array with predicted labels, with shape (num_instances, num_classes) and dtype: uint8
+        Numpy array with true labels, with shape (num_instances, num_classes) and dtype: uint8
 
     Returns
     -------
@@ -83,7 +90,7 @@ def mse(ypred: cp.ndarray, y: cp.ndarray) -> cp.ndarray:
     return cost
 
 
-def mse_derivative(ypred: cp.ndarray, y: cp.ndarray) -> cp.ndarray:
+def mse_derivative(ypred: cp.ndarray, y: cp.ndarray, **kwargs) -> cp.ndarray:
     """
     Derivative for Mean Squared Error
 
@@ -92,7 +99,7 @@ def mse_derivative(ypred: cp.ndarray, y: cp.ndarray) -> cp.ndarray:
     ypred
         Numpy array with predicted labels, with shape (num_instances, num_classes) and dtype: float32
     y
-        Numpy array with predicted labels, with shape (num_instances, num_classes) and dtype: uint8
+        Numpy array with true labels, with shape (num_instances, num_classes) and dtype: uint8
 
     Returns
     -------
@@ -113,7 +120,7 @@ def hinge_loss(ypred: cp.ndarray, y: cp.ndarray) -> cp.ndarray:
     return loss.mean(axis=0)
 
 
-def hinge_derivative(ypred: cp.ndarray, y: cp.ndarray) -> cp.ndarray:
+def hinge_derivative(ypred: cp.ndarray, y: cp.ndarray, **kwargs) -> cp.ndarray:
     assert ypred.shape == y.shape
     assert y.ndim == 2
 