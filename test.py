2ddf05ba4de9c936068e4dfa1cbc0a7b882df86a
Arslan <rslnkrmt2552@gmail.com>
2020-06-04 00:38:48 +0300

    MNIST-C tests

--- test.py
+++ test.py
@@ -6,22 +6,25 @@ from mlpcode.network import Network
 from mlpcode.optim import LRScheduler, LRSchedulerStrat as LRS
 from mlpcode.utils import DATASETS, loadDataset, MODELDIR, split_train_valid
 
-useGpu = False
+useGpu = True
 binarized = False
-dataset = DATASETS.fashion
+dataset = DATASETS.mnistc_spatter
 print("Loading {}".format(dataset))
 trainX, trainY, testX, testY = loadDataset(dataset, useGpu=useGpu)
+
+trainX = trainX * 2 - 1
+testX = testX * 2 - 1
+
 trainX, valX, trainY, valY = split_train_valid(trainX, trainY)
-# Set quant_precision to any integer > 1 to quantize the input,
-# The quantized input having quant_precision + 1 unique elements
-# trainX, trainY, testX, testY = loadDataset(dataset, useGpu=useGpu, quant_precision=2)
+
+
 print("Finished loading {} data".format(dataset))
 
-layers = [trainX.shape[1], 150, 300, 10]
-epochs = 3
-batchSize = 100
-lrStart = 7e-2
-lrEnd = 1e-4
+layers = [trainX.shape[1], 200, 100, 10]
+epochs = 1000
+batchSize = 200
+lrStart = 0.07
+lrEnd = 7e-7
 # lr = 0.07
 lr = LRScheduler(
     alpha=lrStart, decay_rate=(lrStart - lrEnd) ** (1 / epochs), strategy=LRS.exp
@@ -39,7 +42,7 @@ nn = Network(
 # nn = Network.fromModel(modelPath, useGpu=useGpu, binarized=binarized)
 
 # Must compile the model before trying to train it
-nn.compile(lr=lr, hiddenAf=af.leaky_relu, outAf=af.softmax, lossF=lf.cross_entropy)
+nn.compile(lr=lr, hiddenAf=af.sigmoid, outAf=af.softmax, lossF=lf.cross_entropy)
 
 # Save best will switch the model weights and biases to the ones with best accuracy at the end of the training loop
 history = nn.train(
@@ -52,15 +55,13 @@ history = nn.train(
     valY=valY,
 )
 
-# Save will be called separately if we want to save the model
-# Set binarized to true if you want to save the binary version of the weights
 unitsListStr = "_".join(map(str, nn.unitList))
-modelName = f"{dataset}_{unitsListStr}"
+modelName = f"dnn_{dataset}_{unitsListStr}_normalized"
 nn.save_weights(modelName=modelName, binarized=False)
 with open(MODELDIR / f"{modelName}_history.pkl", "wb") as fp:
     pickle.dump(history, fp)
 
-correct = nn.evaluate(testX, testY, batch_size=-1)
+correct = nn.evaluate(testX, testY)
 acc = correct / testX.shape[0] * 100.0
 print(
     "Accuracy on test set:\t{0} / {1} : {2:.03f}%".format(correct, testX.shape[0], acc)
