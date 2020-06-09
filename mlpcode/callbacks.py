51a170f4020b71fd2599e37c7ec1f8c1d81937cc
Arslan <rslnkrmt2552@gmail.com>
2020-06-09 15:53:58 +0300

    Add unpack and repack

--- mlpcode/callbacks.py
+++ mlpcode/callbacks.py
@@ -31,23 +31,51 @@ class ErrorCallback(Callback):
     def _hybridMode(self, inp):
         return inp
 
+    @staticmethod
+    def unpack(inp):
+        buff = np.frombuffer(inp, dtype=np.uint8)
+        unpacked = np.unpackbits(buff).astype(np.bool).reshape(-1, 32)
+        return unpacked
+
+    @staticmethod
+    def repack(inp):
+        buff = np.packbits(inp)
+        packed = np.frombuffer(buff, dtype=np.float32)
+        return packed
+
+
     def _flip(self, inp: np.ndarray):
-        unpacked = np.unpackbits(inp)
+        unpacked = self.unpack(inp)
 
         flipped = self._flipFunc(unpacked)
 
-        packed = np.packbits(flipped)
+        packed = self.repack(flipped)
 
         return packed
 
     def __call__(self, inp: np.ndarray):
         assert inp.ndim == 2
 
-        rowIdx = np.random.choice(inp.shape[0], int(inp.shape[0] * self.pFlip), replace=False)
-        colIdx = np.random.choice(inp.shape[1], int(inp.shape[1] * self.pNeurons), replace=False)
+        rowIdx = np.random.choice(inp.shape[0], round(inp.shape[0] * self.pFlip), replace=False)
+        colIdx = np.random.choice(inp.shape[1], round(inp.shape[1] * self.pNeurons), replace=False)
+
+
+        if rowIdx.size == 0 or colIdx.size == 0:
+            return inp
 
         flipSeq = (rowIdx, colIdx)
+        print(f"Flipping {flipSeq}")
 
         inp[flipSeq] = self._flip(inp[flipSeq])
 
         return inp
+
+
+if __name__ == "__main__":
+    w = np.random.randn(3, 5).astype(np.float32)
+
+    err = ErrorCallback(2, 0.2, 0.3)
+
+    print(w)
+    flippedW = err(w)
+    print(flippedW)
