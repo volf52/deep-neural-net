14701898329556f22bdfe292ecc8b42d2f0f46f0
Arslan <rslnkrmt2552@gmail.com>
2020-07-24 21:13:13 +0300

    Testing Pytorch BNNs (with error injection support)

--- weightsErrorInjectionv2.py
+++ weightsErrorInjectionv2.py
@@ -7,23 +7,28 @@ useGpu = False
 binarized = True
 ds = DATASETS.mnist
 
-modelPth = MODELDIR / "bnn_mnist_1024_1024_yesBN_noBias_98_433_968_98_27.hdf5"
+modelPth = MODELDIR / "bnnPytorchTest.hdf5"
 assert modelPth.exists()
 
 _, _, testX, testY = loadDataset(ds, useGpu=useGpu)
-testX = normalize(testX)
+del _
+testX = normalize(testX, newMin=-1., newMax=1.)
 # pList = [0.01, 0.02, 0.05, 0.1, 0.2]
-pList = [0.2]
+p = 0.1
 
-for p in pList:
-    lst = []
-    # for _ in range(20):
-    nn = Network.fromModel(modelPth, useGpu=useGpu, binarized=binarized)
-    cb = ErrorCallback(p, mode=0, bnn=binarized)
-    nn.compile(hiddenAf=af.sign, outAf=af.identity)
-    nn.addCallbacks(cb)
-    acc = nn.get_accuracy(testX, testY)
-    lst.append(acc)
+nn = Network.fromModel(modelPth, useGpu=useGpu, binarized=binarized)
+nn.compile(hiddenAf=af.sign, outAf=af.softmax)
+acc = nn.get_accuracy(testX, testY)
+print(f"P: {p:0.02f}\tAcc: {acc:0.02f}\n\n")
 
-    acc = sum(lst) / len(lst)
-    print(f"P: {p:0.02f}\tAcc: {acc:0.02f}\n\n")
+cb = ErrorCallback(p, mode=2, bnn=binarized)
+nn.addCallbacks(cb)
+acc = nn.get_accuracy(testX, testY)
+print(f"P: {p:0.02f}\tAcc: {acc:0.02f}\n\n")
+
+nn.clearCallbacks()
+
+cb = ErrorCallback(0.2, mode=2, bnn=binarized)
+nn.addCallbacks(cb)
+acc = nn.get_accuracy(testX, testY)
+print(f"P: {p:0.02f}\tAcc: {acc:0.02f}\n\n")
