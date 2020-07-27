53860181dc8fcfcc765cf8253fcaab588a1c8e78
Arslan <rslnkrmt2552@gmail.com>
2020-07-27 14:33:46 +0300

    Small updates to reqs and test files

--- test.py
+++ test.py
@@ -7,35 +7,32 @@ from mlpcode.optim import LRScheduler, LRSchedulerStrat as LRS
 from mlpcode.utils import DATASETS, loadDataset, MODELDIR, split_train_valid, normalize
 
 useGpu = True
-binarized = False
-dataset = DATASETS.mnistc_spatter
+binarized = True
+dataset = DATASETS.mnist
 print("Loading {}".format(dataset))
 trainX, trainY, testX, testY = loadDataset(dataset, useGpu=useGpu)
 
 trainX = normalize(trainX)
 testX = normalize(testX)
 
-trainX = trainX * 2 - 1
-testX = testX * 2 - 1
-
 trainX, valX, trainY, valY = split_train_valid(trainX, trainY)
 
 print("Finished loading {} data".format(dataset))
 
-layers = [trainX.shape[1], 200, 100, 10]
+layers = [trainX.shape[1], 500, 500, 10]
 epochs = 1000
 batchSize = 200
-lrStart = 0.07
-lrEnd = 7e-7
-# lr = 0.07
-lr = LRScheduler(
-    alpha=lrStart, decay_rate=(lrStart - lrEnd) ** (1 / epochs), strategy=LRS.exp
-)
+lrStart = 0.03
+lrEnd = 3e-7
+lr = 0.01
+# lr = LRScheduler(
+#     alpha=lrStart, decay_rate=(lrStart - lrEnd) ** (1 / epochs), strategy=LRS.exp
+# )
 
 print("\nCreating neural net")
 # Creating from scratch
 nn = Network(
-    layers, useGpu=useGpu, binarized=binarized, useBatchNorm=False, useBias=True
+    layers, useGpu=useGpu, binarized=binarized, useBatchNorm=True, useBias=False
 )
 
 # Creating from a pretrained model
@@ -44,7 +41,7 @@ nn = Network(
 # nn = Network.fromModel(modelPath, useGpu=useGpu, binarized=binarized)
 
 # Must compile the model before trying to train it
-nn.compile(lr=lr, hiddenAf=af.sigmoid, outAf=af.softmax, lossF=lf.cross_entropy)
+nn.compile(lr=lr, hiddenAf=af.sign, outAf=af.softmax, lossF=lf.cross_entropy)
 
 # Save best will switch the model weights and biases to the ones with best accuracy at the end of the training loop
 history = nn.train(
@@ -57,11 +54,11 @@ history = nn.train(
     valY=valY,
 )
 
-unitsListStr = "_".join(map(str, nn.unitList))
-modelName = f"dnn_{dataset}_{unitsListStr}_normalized"
-nn.save_weights(modelName=modelName, binarized=False)
-with open(MODELDIR / f"{modelName}_history.pkl", "wb") as fp:
-    pickle.dump(history, fp)
+# unitsListStr = "_".join(map(str, nn.unitList))
+# modelName = f"bnn_{dataset}_{unitsListStr}_bnnNew"
+# nn.save_weights(modelName=modelName, binarized=False)
+# with open(MODELDIR / f"{modelName}_history.pkl", "wb") as fp:
+#     pickle.dump(history, fp)
 
 correct = nn.evaluate(testX, testY)
 acc = correct / testX.shape[0] * 100.0
--- weightsErrorInjectionv2.py
+++ weightsErrorInjectionv2.py
@@ -7,21 +7,20 @@ useGpu = False
 binarized = True
 ds = DATASETS.mnist
 
-modelPth = MODELDIR / "bnnPytorchCheckpTest.hdf5"
+modelPth = MODELDIR / "bnn_pytorch_1000Ep_PYTOR-7.hdf5"
 assert modelPth.exists()
 
 _, _, testX, testY = loadDataset(ds, useGpu=useGpu)
 del _
 testX = normalize(testX, newMin=-1., newMax=1.)
 # pList = [0.01, 0.02, 0.05, 0.1, 0.2]
-p = 0.1
 
 nn = Network.fromModel(modelPth, useGpu=useGpu, binarized=binarized)
 nn.compile(hiddenAf=af.sign, outAf=af.softmax)
 acc = nn.get_accuracy(testX, testY)
 print(f"P: None\tAcc: {acc:0.02f}")
 
-cb = ErrorCallback(p, mode=2, bnn=binarized)
+cb = ErrorCallback(0.1, mode=2, bnn=binarized)
 nn.addCallbacks(cb)
 acc = nn.get_accuracy(testX, testY)
 print(f"P: 0.1\tAcc: {acc:0.02f}")
